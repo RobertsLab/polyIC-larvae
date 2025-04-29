7a02729c74108f426783045389007de02bf49a9c
madsmarine <mbaird2827@gmail.com>
2025-04-28 19:03:58 -0700

    growth r script

---
+++ scripts/growth/Untitled.R
@@ -0,0 +1,12 @@
+title: #PolyIC oyster growth ImageJ anaylsis  
+author: "MM Baird"
+date: '2025'
+
+# Set up workspace and load packages 
+
+
+# Load Data
+
+# Turn Into Long Format
+
+# Analyze growth over time series 
